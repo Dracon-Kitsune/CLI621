47c687f5f8041d0f478f596d7807ac3318b6acaa
Maxine Red <maxine_red1@yahoo.com>
2014-08-25 10:13:06 +0200

    Started to implement sets.

---
+++ lib/set/main.rb
@@ -0,0 +1,35 @@
+=begin
+  Copyright 2014 Maxine Red <maxine_red1@yahoo.com>
+
+  This file is part of CLI621.
+
+  CLI621 is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  CLI621 is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with CLI621.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
+module E621
+  class Main
+    private
+    # Do module specific tasks here.
+    def mod_init
+      Set.init(@config,@paths)
+      Post.init(@config,@paths)
+      Readline.completion_proc = proc do |s|
+        # A function to get some useful completion feed back should be here.
+      end
+      Readline.completer_word_break_characters  = " "
+      Readline.completion_append_character      = " "
+      @prompt = "/pool"
+    end
+  end
+end
